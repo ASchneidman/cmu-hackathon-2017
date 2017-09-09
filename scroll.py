207f356e2fa21237c92998d2e9e8bf22891474da
pi314mm <matias.scharager@gmail.com>
2017-09-09 13:32:26 -0400

    scroll!

--- scroll.py
+++ scroll.py
@@ -7,16 +7,17 @@ from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome('C:\\Users\\matia\\Documents\\GitHub\\cmu-hackathon-2017\\chromedriver.exe')
 driver.get("https://twitter.com/realDonaldTrump/status/906320446882271232")
-time.sleep(5)
 
-print("hi")
 
-element = driver.find_element_by_xpath('/html/body')
+element = driver.find_element_by_class_name('permalink-tweet')
 
 #element.send_keys(Keys.CONTROL , Keys.END)
 for x in range(50):
-    element.send_keys(Keys.PAGE_DOWN)
+    for y in range(3):
+        element.send_keys(Keys.SPACE)
+    time.sleep(1)
 #driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+#driver.execute_script("window.scrollBy(0, 500);")
 
 post_elems = driver.find_elements_by_class_name("tweet-text")
 
