c0c76b00740a9b12ffbc0ccbd5ebff6b95f7a5cf
Wert5 <Wert5@users.noreply.github.com>
2017-09-09 14:50:17 -0400

    Make GetReplies Function
    Cant test because chromedriver only works on windows.

--- scroll.py
+++ scroll.py
@@ -5,23 +5,27 @@ import selenium
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Chrome('C:\\Users\\matia\\Documents\\GitHub\\cmu-hackathon-2017\\chromedriver.exe')
-driver.get("https://twitter.com/realDonaldTrump/status/906320446882271232")
+
+def getReplies(tweetLink):
+    driver = webdriver.Chrome('/Users/winstongrenier/Documents/GitHub/cmu-hackathon-2017/chromedriver.exe')
+    driver.get(tweetLink)
+
+    element = driver.find_element_by_class_name('permalink-tweet')
+
+    #lastHeight = element.pagesourse
+    for x in range(50):
+        element.send_keys(Keys.CONTROL, Keys.END)
+        time.sleep(1)
+        #if(lastHeight==element.scrollHeight):
+        #    break
+        #lastHeight=element.scrollHeight
+
+    post_elems = driver.find_elements_by_class_name("tweet-text")
+
+    for post in post_elems:
+        print(post.text)
+
+    driver.quit()
 
 
-element = driver.find_element_by_class_name('permalink-tweet')
-
-#lastHeight = element.pagesourse
-for x in range(50):
-    element.send_keys(Keys.CONTROL , Keys.END)
-    time.sleep(1)
-    #if(lastHeight==element.scrollHeight):
-    #    break
-    #lastHeight=element.scrollHeight
-
-post_elems = driver.find_elements_by_class_name("tweet-text")
-
-for post in post_elems:
-    print(post.text)
-
-driver.quit()
+getReplies("https://twitter.com/realDonaldTrump/status/906320446882271232")
