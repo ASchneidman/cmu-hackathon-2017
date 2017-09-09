085c9fe19d495a646e102c764705b010c29ecfa1
Wert5 <Wert5@users.noreply.github.com>
2017-09-08 21:35:35 -0400

    Getting Replies

--- twitterTest.py
+++ twitterTest.py
@@ -1,5 +1,6 @@
 import tweepy
-
+import json
+import sys
 # Go to http://apps.twitter.com and create an app.
 # The consumer key and secret will be generated for you after
 consumer_key = "8TIPnyB7k3CRWS2gYCtd7GRIy"
@@ -10,9 +11,16 @@ consumer_secret = "EoQMo54FyWN4yxwtJIPSY2MnoiNLR8CXn2YLUITCpZmoDEu3Tf"
 access_token = "906313272680501248-EM6nDSoY0rbNgHwjVre2T1c0itpdkRb"
 access_token_secret = "JRUdz2vtomDWbWjFjDhjshS65fUOTtVDldaJXRO3pHrHu"
 
+non_bmp_map = dict.fromkeys(range(0x10000, sys.maxunicode + 1), 0x12)
+
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-print(api.statuses_lookup([906282980653617152])[0].text)
+tweetId = 906318327508848640
+
+print(api.statuses_lookup([tweetId])[0].text.translate(non_bmp_map))
+for result in api.search("to:@DavidLawTennis", sinceId=tweetId, count=500):
+    if(result.in_reply_to_status_id == tweetId):
+        print(result.text.translate(non_bmp_map))
